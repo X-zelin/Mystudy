b8864a262ed2782c9355b3ca8dfaca7299979b69
X-zelin <3533459261@qq.com>
2024-11-20 13:13:05 +0800

    2024/11/20

---
+++ Learning/src/com/xv/LeetCode/TreeNode/LeetCode101.java
@@ -0,0 +1,22 @@
+package com.xv.LeetCode.TreeNode;
+
+
+import com.xv.DataStruction.binarytree.TreeNode;
+
+public class LeetCode101 {
+    public boolean isSymmetric(TreeNode root){
+        return check(root.left,root.right);
+    }
+    private boolean check(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null ||right == null){
+            return false;
+        }
+        if(left.val !=right.val){
+            return false;
+        }
+        return check(left.left,right.right)&&check(left.right,right.left);
+    }
+}
